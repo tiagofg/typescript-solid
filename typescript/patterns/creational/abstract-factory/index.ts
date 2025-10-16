00543f6da2e5f773d4fc9da2aff2d2cbda86e53e
Tiago Feliciano Gomes <tfelicianogomes@gmail.com>
2025-10-16 11:09:56 -0300

    feat: creating abstract factory

---
+++ typescript/patterns/creational/abstract-factory/index.ts
@@ -0,0 +1,30 @@
+interface IProductA {
+  operationA(): string;
+}
+
+interface IProductB {
+  operationB(): string;
+  combinedOperation(other: IProductA): string;
+}
+
+interface IFactory {
+  createProductA(): IProductA;
+  createProductB(): IProductB;
+}
+
+class ProductA implements IProductA {
+  operationA(): string {
+    return 'Result of ProductA1';
+  }
+}
+
+class ProductB implements IProductB {
+  operationB(): string {
+    return 'Result of ProductB1';
+  }
+
+  combinedOperation(other: IProductA): string {
+    const result = other.operationA();
+    return `ProductB1 combines with (${result})`;
+  }
+}
